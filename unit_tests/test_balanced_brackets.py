386b09017cd52dc52765967a59888759647df30a
Tay Cong Run <congrun@hotmail.com>
2022-05-09 16:37:27 +0800

    Solution & test cases for https://www.hackerrank.com/challenges/balanced-brackets/problem

---
+++ unit_tests/test_balanced_brackets.py
@@ -0,0 +1,28 @@
+import unittest
+
+from balanced_brackets import isBalanced
+
+
+class BalancedBracketsTestCase(unittest.TestCase):
+    def test_balanced_brackets(self):
+        for s in (
+            r'{[()]}', 
+            r'{{[[(())]]}}',
+            r'({}[])',
+        ):
+            with self.subTest(s=s):
+                self.assertEqual(isBalanced(s), 'YES')
+
+    def test_unbalanced_brackets(self):
+        for s in (
+            r'{[(])}',
+            r'{][)()]}',
+        ):
+            with self.subTest(s=s):
+                self.assertEqual(isBalanced(s), 'NO')
+
+    def test_all_open_brackets(self):
+        self.assertEqual(isBalanced('([{([[[[[(({'), 'NO')
+
+    def test_all_closed_brackets(self):
+        self.assertEqual(isBalanced(']]]))]}'), 'NO')
